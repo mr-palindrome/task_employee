45441d705e4ac171d520402774cacb5fc301c0f2
Nayan <ndas5144@gmail.com>
2022-04-11 14:05:58 +0530

    changed list display in admin

--- employee/admin.py
+++ employee/admin.py
@@ -4,4 +4,4 @@ from .models import EmployeeData
 # Register your models here.
 @admin.register(EmployeeData)
 class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ['emp_no']
+    list_display = ('emp_no', 'first_name', 'last_name', 'hire_date')
